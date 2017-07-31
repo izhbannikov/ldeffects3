a73ce70108b3bfb4ad297a021d439a16957365e3
izhbannikov <zhbannikov.ilya@gmail.com>
2017-07-31 12:05:43 -0400

    Minor bug fixes, 3D plot moved to separate tab, Q21 was removed.

--- server.R
+++ server.R
@@ -12,10 +12,10 @@ shinyServer(function(input, output, session) {
   {
     Q11 <- parms[1]
     Q12 <- parms[2]
-    Q21 <- parms[3]
-    Q22 <- parms[4]
-    g01 <- parms[5]
-    g02 <- parms[6]
+    Q21 <- Q12
+    Q22 <- parms[3]
+    g01 <- parms[4]
+    g02 <- parms[5]
     
     m1 <- y[1]
     m2 <- y[2]
@@ -39,39 +39,113 @@ shinyServer(function(input, output, session) {
   }
   
   data <- reactive({
+      
+      maxval <- max(c(input$gamma110, input$gamma220))
+      if(input$gamma120 > maxval)
+      {
+          updateSliderInput(session, "gamma120", value=maxval)
+      }
+    
       yini <- c(y1 = input$m10, y2 = input$m20, y3 = input$gamma110, y4 = input$gamma120, y5 = input$gamma220)
       res <- ode(y = yini, func = func,
-               times = input$time[1]:input$time[2], parms = c(input$Q11, input$Q12, input$Q21, input$Q22, 
+               times = input$time[1]:input$time[2], parms = c(input$Q11, input$Q12, input$Q22, 
                                                               input$g01, input$g02))
     
       colnames(res) <- c("time", "m1", "m2", "gamma11", "gamma12", "gamma22")
-      res
+      
+      t <- input$t3d - input$time[1] + 1
+      if(t < 0 | t > input$time[2] - input$time[1] + 1)
+      {
+        stop("Error: t out of age range!")
+      }
+      
+      mu <- c(res[t,2], res[t,3])
+      Sigma <- matrix(c(res[t,4], res[t,5], 
+                        res[t,5], res[t,6]), 2)
+      
+      rho <- 0
+      mu_x <- mu[1]
+      mu_y <- mu[2]
+      sigma_x <- (Sigma[1,1])^0.5
+      sigma_y <- (Sigma[2,2])^0.5
+      
+      xx <- seq(-3,3,length.out = 100)
+      yy <- seq(-3,3,length.out = 100)
+      
+      z <- matrix(nrow=length(xx), ncol=length(yy), NA)
+      
+      for(i in 1:length(xx))
+      {
+          for(j in 1:length(yy))
+          {
+              x <- xx[i]
+              y <- yy[j]
+              fxy <- 1/(2*pi*sigma_x*sigma_y*(1-rho^2)^0.5)*exp((-1/2)*((x - mu_x)^2/sigma_x^2 + (y - mu_y)^2/sigma_y^2))
+              z[i,j] <- fxy
+          }
+      }
+      
+      list(x=x,y=y,z=z,res=res)
+      
   })
   
-  mPlot <- function(cols=1, save=F){
+  PlotMain <- function(cols=1, save=F){
     res <- data()
-    plot(res)
+    plot(res$res)
   }
   
-  mPlot2 <- function(cols=1, save=F){
+  PlotDensity <- function(cols=1, save=F){
     res <- data()
-    t <- input$t3d
-    bivn <- mvrnorm(input$N, mu = c(res[t,2], res[t,3]), 
-                    Sigma = matrix(c(res[t,4], res[t,5], 
-                                     res[t,5], res[t,6]), 2))
-    bivn.kde <- kde2d(bivn[,1], bivn[,2], n = 50)
-    persp(bivn.kde, phi = 45, theta = 30, ticktype="detailed",
+    persp(z=res$z, phi = 45, theta = 30, ticktype="detailed",
           xlab="x", ylab="y", zlab="z", axes=T)
   }
 
   
+  PlotContour <- function(cols=1, save=F){
+    res <- data()
+    contour(z=res$z)
+  }
+  
+  
   output$distPlot <- renderPlot({
-    print(mPlot())
+    print(PlotMain())
   })
   
   output$distPlot3d <- renderPlot({
-    print(mPlot2())
+    print(PlotDensity())
   })
+  
+  output$countourPlot <- renderPlot({
+      print(PlotContour())
+  })
+  
+  output$savePlotMain <- downloadHandler(
+    filename = "plotMain.png",
+    content = function(file) {
+      png(file, width = 1024, height = 1024)
+      print(PlotMain())
+      dev.off()
+    }
+  ) 
+  
+  output$savePlotDensity <- downloadHandler(
+    filename = "plotDensity.png",
+    content = function(file) {
+      png(file, width = 1024, height = 1024)
+      print(PlotDensity())
+      dev.off()
+    }
+  )  
+  
+  output$savePlotContour <- downloadHandler(
+    filename = "plotContour.png",
+    content = function(file) {
+      png(file, width = 1024, height = 1024)
+      print(PlotContour())
+      dev.off()
+    }
+  )   
+  
 
   
 })
--- ui.R
+++ ui.R
@@ -7,33 +7,24 @@ shinyUI(fluidPage(
   fluidRow(
     column(4,
            wellPanel(
-             h4("Time (age range)"),
-             sliderInput("time",
-                  "t:",
-                  min = 1,
-                  max = 120,
-                  value = c(30,105))),
-           wellPanel(
-             h4("T: time for 3D plot and number of samples: N"),
-             sliderInput("t3d","t:", min = 1,max = 120,value = 50, step=1),
-             sliderInput("N","N:", min = 1,max = 10000,value = 1000, step=1)
+             sliderInput("time", "Aage range", min = 1, max = 120, value = c(30,105)),
+             sliderInput("t3d","Age", min = 1,max = 120,value = 60, step=1)
            ),
            wellPanel(
-             h4("Constant values"),
-             sliderInput("Q11","Q11:", min = 0,max = 1e-3,value = 0.5e-3, step=1e-5),
-             sliderInput("Q12", "Q12:", min = 0, max = 1e-4, value = 0.5e-4, step=1e-6),
-             sliderInput("Q21", "Q21:", min = 0, max = 1e-4, value = 0.5e-4, step=1e-6),
-             sliderInput("Q22", "Q22:", min = 0, max = 1e-3, value = 0.5e-3, step=1e-5),
+             h4("Constants"),
+             sliderInput("Q11","Q11:", min = 0,max = 2,value = 0.5e-1, step=1e-5),
+             sliderInput("Q12", "Q12:", min = 0, max = 1, value = 0.5e-2, step=1e-6),
+             sliderInput("Q22", "Q22:", min = 0, max = 2, value = 0.5e-1, step=1e-5),
              sliderInput("g01", "g01:", min = 0, max = 1, value = 0, step = 1),
              sliderInput("g02", "g02:", min = 0, max = 1, value = 1, step=1)
              ),
            wellPanel(
              h4("Initial values"),
-             sliderInput("m10","m1(0):", min = 0,max = 5,value = 1),
-             sliderInput("m20", "m2(0):", min = 0, max = 5, value = 2),
-             sliderInput("gamma110", "gamma11(0):", min = 0, max = 1, value = 0.5),
-             sliderInput("gamma120", "gamma12(0):", min = 0, max = 1, value = 0.05),
-             sliderInput("gamma220", "gamma22(0):", min = 0, max = 1, value = 0.5)
+             sliderInput("m10","m1(0):", min = 0,max = 5,value = 0, step=1e-4),
+             sliderInput("m20", "m2(0):", min = 0, max = 5, value = 0, step=1e-4),
+             sliderInput("gamma110", "gamma11(0):", min = 0, max = 2, value = 0.5, step=1e-4),
+             sliderInput("gamma120", "gamma12(0):", min = 0, max = 2, value = 0.1, step=1e-4),
+             sliderInput("gamma220", "gamma22(0):", min = 0, max = 2, value = 0.5, step=1e-4)
            )
            
     ),
@@ -42,9 +33,14 @@ shinyUI(fluidPage(
     mainPanel(
       tabsetPanel(
         tabPanel("Main Plots", 
-                 plotOutput("distPlot",height = 1024)),
-        tabPanel("3D density plot from 2D bivariate normal distribution", 
-                 plotOutput("distPlot3d",height = 1024))
+                 plotOutput("distPlot",height = 1024),
+                 downloadButton("savePlotMain", label="Save")),
+        tabPanel("Contour plot", 
+                 plotOutput("countourPlot", height = 1024),
+                 downloadButton("savePlotContour", label="Save")),
+        tabPanel("3D density plot", 
+                 plotOutput("distPlot3d",height = 1024),
+                 downloadButton("savePlotDensity", label="Save"))
       )
       
     )
